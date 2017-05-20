5598f23a1fe5a25fdd5eee64a0de1e62b4cfff2c
Sydney Sprouse <sydneysprouse@Sydneys-MacBook-Air.local>
2017-05-20 11:59:13 -0600

    finished

--- practice.js
+++ practice.js
@@ -32,47 +32,50 @@ var user = {
 };
 //Now, invoke the getUsername method and verify you got the username of the object and not anything else.
 
-user.username();
+user.getUsername();
 
 
 // Write a constructor function, including method definitions, which will make the following function invocations function properly.
 
-function Car (make, model, year){
-  this.make = make;
-  this.model = model;
-  this.year = year;
+function Car (make, model, year) {
+  this.make = make,
+  this.model = model,
+  this.year = year,
+  this.move = 0,
+  this.moveCar = function () {
+    return this.move += 10
+  }
 };
 
 var prius = new Car('Toyota', 'Prius', 2011);
 var mustang = new Car('Ford', 'Mustang', 2013);
 
 //Hint, you'll need to add a move property, with a starting value of zero, and write a moveCar function which will increment the move property by 10. The move property will be added to every object that is being returned from the Car function. You'll also need to use the 'this' keyword properly in order to make sure you're invoking moveCar on the right object (prius vs mustang).
-
-Car.move = 0;
-
-var moveCar = function () {
-  return this.move + 10;
-}
+// Car.prototype.move = 0;
+// Car.prototype.moveCar = function () {
+//   return this.move += 10;
+// };
 
 
 
-prius.moveCar(); //increments prius' move property by 10. Returns the new move property.
-mustang.moveCar(); //increments mustang' move property by 10. Returns the new move property.
 
+//increments mustang' move property by 10. Returns the new move property.
+prius.moveCar();
+mustang.moveCar();
 
 
 //Continuation of previous problem
 
-var getYear = function(){
+var getYear = function() {
   return this.year;
 };
 
-prius.getYear();
-mustang.getYear();
 //Above you're given the getYear function. Call the getYear function with the prius then the mustang objects being the focal objects. *Don't add getYear as a property on both objects*.
 
 //Note(no tests)
-  //Code Here
+
+getYear.call(prius);
+getYear.call(mustang);
 
 
 //New Problem
@@ -84,11 +87,13 @@ var myUser = {
 };
 
 var getMyUsername = function() {
+  console.log("this is this", this)
  return this.username;
 };
 
 var userName = getMyUsername.call(myUser);
-userName;
+// userName;
+
 
 //Above you're given an object, and  a function. What will the getMyUsername function return?
 //Note(no tests)
